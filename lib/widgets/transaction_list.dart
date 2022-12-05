e6b8a0b3fbbb6636cfbdb4acfd3b6552e8e7aab5
dondonjedd <70884225+dondonjedd@users.noreply.github.com>
2022-12-05 20:21:45 +0800

    create slidable delete button

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:intl/intl.dart';
 import '../models/transaction.dart';
 
@@ -29,62 +30,67 @@ class TransactionList extends StatelessWidget {
             )
           : ListView.builder(
               itemBuilder: (ctx, index) {
+                // return Slidable(
+                //   direction: Axis.horizontal,
+                //   closeOnScroll: true,
+                //   endActionPane: const ActionPane(
+                //     extentRatio: 0.2,
+                //     motion: ScrollMotion(),
+                //     children: [
+                //       SlidableAction(
+                //         borderRadius: BorderRadius.all(Radius.circular(10)),
+                //         onPressed: null,
+                //         backgroundColor: Color(0xFFFE4A49),
+                //         foregroundColor: Colors.white,
+                //         icon: Icons.delete,
+                //         label: 'Delete',
+                //       ),
+                //     ],
+                //   ),
+                // child:
                 return Card(
                   elevation: 5,
                   margin:
                       const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                        backgroundColor: Theme.of(context).colorScheme.primary,
-                        radius: 30,
-                        child: Padding(
-                          padding: const EdgeInsets.all(6.0),
-                          child: FittedBox(
-                            child: Text(
-                                "RM${transactions[index].amount.toStringAsFixed(2)}"),
-                          ),
-                        )),
-                    title: Text(transactions[index].title,
-                        style: Theme.of(context).textTheme.titleLarge),
-                    subtitle: Text(
-                      DateFormat.yMMMd().format(transactions[index].date),
-                      style: const TextStyle(fontSize: 12, color: Colors.grey),
+                  child: Slidable(
+                    direction: Axis.horizontal,
+                    closeOnScroll: true,
+                    endActionPane: const ActionPane(
+                      extentRatio: 0.2,
+                      motion: ScrollMotion(),
+                      children: [
+                        SlidableAction(
+                          onPressed: null,
+                          backgroundColor: Color(0xFFFE4A49),
+                          foregroundColor: Colors.white,
+                          icon: Icons.delete,
+                          label: 'Delete',
+                        ),
+                      ],
                     ),
-                  ),
-                  //     child: Row(
-                  //   mainAxisAlignment: MainAxisAlignment.start,
-                  //   children: [
-                  //     Container(
-                  //         margin: const EdgeInsets.symmetric(
-                  //           vertical: 10,
-                  //           horizontal: 15,
-                  //         ),
-                  //         decoration: BoxDecoration(
-                  //             border: Border.all(
-                  //                 color: Theme.of(context).colorScheme.primary,
-                  //                 width: 2)),
-                  //         padding: const EdgeInsets.all(10),
-                  //         child: Text(
-                  //           "RM${transactions[index].amount.toStringAsFixed(2)}",
-                  //           style: TextStyle(
-                  //               fontWeight: FontWeight.bold,
-                  //               fontSize: 20,
-                  //               color: Theme.of(context).colorScheme.primary),
-                  //         )),
-                  //     Column(
-                  //       crossAxisAlignment: CrossAxisAlignment.start,
-                  //       children: [
-                  //         Text(transactions[index].title,
-                  //             style: Theme.of(context).textTheme.titleLarge),
-                  //         Text(
-                  //           DateFormat.yMMMd().format(transactions[index].date),
-                  //           style:
-                  //               const TextStyle(fontSize: 12, color: Colors.grey),
-                  //         )
-                  //       ],
-                  //     )
-                  //   ],
-                  // )
+                    child: ListTile(
+                      leading: CircleAvatar(
+                          backgroundColor:
+                              Theme.of(context).colorScheme.primary,
+                          radius: 30,
+                          child: Padding(
+                            padding: const EdgeInsets.all(6.0),
+                            child: FittedBox(
+                              child: Text(
+                                  "RM${transactions[index].amount.toStringAsFixed(2)}"),
+                            ),
+                          )),
+                      title: Text(transactions[index].title,
+                          style: Theme.of(context).textTheme.titleLarge),
+                      subtitle: Text(
+                        DateFormat.yMMMd().format(transactions[index].date),
+                        style:
+                            const TextStyle(fontSize: 12, color: Colors.grey),
+                      ),
+                      // trailing: const IconButton(
+                      //     icon: Icon(Icons.delete), onPressed: null),
+                    ),
+                  ), //to add slidable
                 );
               },
               itemCount: transactions.length,
