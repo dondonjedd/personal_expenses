4247fbcdffb8d65e641e129ed4c4ecdc1da77da4
dondonjedd <70884225+dondonjedd@users.noreply.github.com>
2022-12-06 22:48:35 +0800

    change size of currency text

--- lib/widgets/chartBar.dart
+++ lib/widgets/chartBar.dart
@@ -6,44 +6,64 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(
-          height: 20,
-          child: FittedBox(
-              child: Text("RM${(tx["amount"] as double).toStringAsFixed(0)}")),
-        ),
-        SizedBox(
-          height: 80,
-          width: 10,
-          child: Stack(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                    color: Colors.grey,
-                    border: Border.all(color: Colors.grey),
-                    borderRadius: BorderRadius.circular(10)),
-              ),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: FractionallySizedBox(
-                  heightFactor: tx["percentage"] as double <= 0
-                      ? 0
-                      : tx["percentage"] as double,
-                  child: Container(
-                    decoration: BoxDecoration(
-                        color: Theme.of(context).colorScheme.secondary,
-                        border: Border.all(
-                            color: Theme.of(context).colorScheme.secondary),
-                        borderRadius: BorderRadius.circular(10)),
-                  ),
+    return LayoutBuilder(builder: (context, constraints) {
+      return Column(
+        children: [
+          SizedBox(
+            height: constraints.maxHeight * 0.15,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const Flexible(flex: 1, child: FittedBox(child: Text("RM"))),
+                Flexible(
+                  flex: 3,
+                  child: FittedBox(
+                      child: Text((tx["amount"] as double).toStringAsFixed(0))),
                 ),
-              )
-            ],
+              ],
+            ),
           ),
-        ),
-        Text(tx["day"] as String),
-      ],
-    );
+          SizedBox(
+            height: constraints.maxHeight * 0.1,
+          ),
+          SizedBox(
+            height: constraints.maxHeight * 0.6,
+            width: 10,
+            child: Stack(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.grey,
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(10)),
+                ),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: FractionallySizedBox(
+                    heightFactor: tx["percentage"] as double <= 0
+                        ? 0
+                        : tx["percentage"] as double,
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Theme.of(context).colorScheme.secondary,
+                          border: Border.all(
+                              color: Theme.of(context).colorScheme.secondary),
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: constraints.maxHeight * 0.05,
+          ),
+          SizedBox(
+              height: constraints.maxHeight * 0.1,
+              child: FittedBox(child: Text(tx["day"] as String))),
+        ],
+      );
+    });
   }
 }
